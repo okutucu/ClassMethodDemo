66468993e7fcd599b32d44fa6ab3dcfbcd749193
okutucu <oktc95@gmail.com>
2021-08-25 10:21:13 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void add(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Adi +" isimli müşteri başarılı bir şekilde eklenmiştir.");
+
+
+        }
+        public void delete(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Adi +  "isimli müşteri başarılı bir şekilde silinmiştir.");
+
+
+        }
+        public void Listele (Musteri[] musteri)
+        {
+
+            foreach (var m in musteri)
+            {
+
+                Console.WriteLine(m.Id + " " +  m.Adi + " " + m.Soyadi + " " + m.DogumYili + " " + m.Meslek);
+
+            }
+
+
+        }
+            
+            
+
+
+
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Adi = "Oğuzhan";
+            musteri1.Id = 1;
+            musteri1.Soyadi = "Kutucu";
+            musteri1.DogumYili = 1995;
+            musteri1.Meslek = "Developer";
+
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Adi = "Kaan";
+            musteri2.Id = 2;
+            musteri2.Soyadi = "Kutucu";
+            musteri2.DogumYili = 1994;
+            musteri2.Meslek = "Grafiker";
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.Adi = "Veli";
+            musteri3.Id = 3;
+            musteri3.Soyadi = "Keresteci";
+            musteri3.DogumYili = 2001;
+            musteri3.Meslek = "Marangoz";
+
+
+            Musteri[] musteri = new Musteri[] {musteri1 , musteri2, musteri3};
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+
+            musteriManager.add(musteri1);
+
+            musteriManager.delete(musteri3);
+
+
+            musteriManager.Listele(musteri);
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
